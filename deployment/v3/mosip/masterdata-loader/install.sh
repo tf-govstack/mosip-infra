ef5602d10f3dddd514a7bf8dbb4c2d0df2ee9619
pramod-technoforte <pramod.belal@technoforte.co.in>
2023-11-27 12:35:01 +0530

    Updated all changes of helm repo to take latest changes from helm for govstack.

--- deployment/v3/mosip/masterdata-loader/install.sh
+++ deployment/v3/mosip/masterdata-loader/install.sh
@@ -33,7 +33,7 @@ if [ $yn = "Y" ]
    ./copy_secrets.sh
 
    echo Loading masterdata
-   helm -n $NS install masterdata-loader  tf-govstack/masterdata-loader --set mosipDataGithubBranch=1.2.0.1 --version $CHART_VERSION --wait
+   helm -n $NS install masterdata-loader  tf-govstack/masterdata-loader --set mosipDataGithubBranch=tf-develop --version $CHART_VERSION --wait
 
    else
    break
