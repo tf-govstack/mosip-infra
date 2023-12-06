0f7f9e78b89a6dee5c31170a10f90edba7acabe8
pramod-technoforte <pramod.belal@technoforte.co.in>
2023-12-06 17:04:36 +0530

    Updated install.sh scripts

--- deployment/v3/mosip/artifactory/install.sh
+++ deployment/v3/mosip/artifactory/install.sh
@@ -18,7 +18,7 @@ function installing_artifactory() {
   helm repo update
 
   echo Installing artifactory
-  helm -n $NS install artifactory mosip/artifactory --set image.repository=tfgovstackdev/artifactory-server --set image.tag=tf-develop --version $CHART_VERSION
+  helm -n $NS install artifactory mosip/artifactory --set image.repository=technogovstack/artifactory-server --set image.tag=release-1.2.0.1.3 --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
